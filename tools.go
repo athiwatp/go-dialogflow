b0b47b7db8f474501e915aff8d270154b7ce69a6
xfrr <francisco.romero.1994@gmail.com>
2019-02-12 11:45:59 +0100

    init

---
+++ tools.go
@@ -0,0 +1,137 @@
+package tools
+
+import (
+	"net/http"
+
+	"github.com/golang/protobuf/jsonpb"
+
+	_struct "github.com/golang/protobuf/ptypes/struct"
+	"google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
+)
+
+// NewTextMessageCollection Creates new message collection
+func NewTextMessageCollection(messages []string) []*dialogflow.Intent_Message {
+	msgs := []*dialogflow.Intent_Message{}
+
+	for i := 0; i < len(messages); i++ {
+		msgs = append(msgs, &dialogflow.Intent_Message{
+			Message: NewTextMessage([]string{
+				messages[i],
+			}),
+		})
+	}
+
+	return msgs
+}
+
+// FindContext Finds context on given collection
+func FindContext(session string, name string, contexts []*dialogflow.Context) *dialogflow.Context {
+	var result *dialogflow.Context
+	fullname := session + "/contexts/" + name
+	for _, context := range contexts {
+		if context.Name == fullname {
+			result = context
+			break
+		}
+	}
+	return result
+}
+
+// FindParameter Finds parameter by name on given parameter struct
+func FindParameter(name string, parameters *_struct.Struct) *_struct.Value {
+	var result *_struct.Value
+	pfields := parameters.GetFields()
+	if val, ok := pfields[name]; ok {
+		result = val
+	}
+	return result
+}
+
+// NewTextMessage Creates new simple text message
+func NewTextMessage(messages []string) *dialogflow.Intent_Message_Text_ {
+	return &dialogflow.Intent_Message_Text_{
+		Text: &dialogflow.Intent_Message_Text{
+			Text: messages,
+		},
+	}
+}
+
+// NewCardButton Creates new card button
+func NewCardButton(text string, postback string) *dialogflow.Intent_Message_Card_Button {
+	return &dialogflow.Intent_Message_Card_Button{Text: text, Postback: postback}
+}
+
+// NewCardMessage Creates card message
+func NewCardMessage(buttons []*dialogflow.Intent_Message_Card_Button, title, subtitle, imageURI string) *dialogflow.Intent_Message_Card_ {
+	return &dialogflow.Intent_Message_Card_{
+		Card: &dialogflow.Intent_Message_Card{
+			Title:    title,
+			Subtitle: subtitle,
+			ImageUri: imageURI,
+			Buttons:  buttons,
+		},
+	}
+}
+
+// NewParameters Creates parameters collection
+func NewParameters(keyv map[string]*_struct.Value) *_struct.Struct {
+	parameters := &_struct.Struct{
+		Fields: keyv,
+	}
+	return parameters
+}
+
+// NewTextValue Creates new text value
+func NewTextValue(value string) *_struct.Value {
+	val := &_struct.Value{
+		Kind: &_struct.Value_StringValue{
+			StringValue: value,
+		},
+	}
+	return val
+}
+
+// NewBoolValue Creates new bool value
+func NewBoolValue(value bool) *_struct.Value {
+	val := &_struct.Value{
+		Kind: &_struct.Value_BoolValue{
+			BoolValue: value,
+		},
+	}
+	return val
+}
+
+// NewNumberValue Creates new number value
+func NewNumberValue(value float64) *_struct.Value {
+	val := &_struct.Value{
+		Kind: &_struct.Value_NumberValue{
+			NumberValue: value,
+		},
+	}
+	return val
+}
+
+// NewContext Creates new context
+func NewContext(session string, name string, lifespan int32, parameters *_struct.Struct) *dialogflow.Context {
+	path := "/contexts/"
+	fullname := session + path + name
+	context := &dialogflow.Context{
+		Name:          fullname,
+		LifespanCount: lifespan,
+		Parameters:    parameters,
+	}
+	return context
+}
+
+// RespondJSON Sends json response back to dialogflow
+func RespondJSON(w http.ResponseWriter, r *dialogflow.WebhookResponse) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	Respond(w, r)
+}
+
+// Respond Sends response back to dialogflow
+func Respond(w http.ResponseWriter, response *dialogflow.WebhookResponse) {
+	jmarshaller := jsonpb.Marshaler{}
+	jmarshaller.Marshal(w, response)
+}
---
+++ tools_test.go
@@ -0,0 +1,97 @@
+package tools
+
+import (
+	"testing"
+
+	_struct "github.com/golang/protobuf/ptypes/struct"
+	"google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
+)
+
+const session = "dfv2ef2eccrfbr3bfv1e11ed1evef1f1"
+
+var contexts = []*dialogflow.Context{}
+var parameters *_struct.Struct
+
+func TestNewParameters(testing *testing.T) {
+	parameters = NewParameters(map[string]*_struct.Value{
+		"name": NewTextValue("test"),
+	})
+
+	if parameters == nil || parameters.Fields == nil {
+		testing.Fatal("invalid parameters created")
+	}
+}
+
+func TestNewContext(testing *testing.T) {
+	ctx := NewContext(session, "testing", 5, parameters)
+	if ctx == nil {
+		testing.Fatal("invalid context created")
+	}
+
+	contexts = append(contexts, ctx)
+}
+
+func TestFindContext(testing *testing.T) {
+	ctx := FindContext(session, "testing", contexts)
+	if ctx == nil {
+		testing.Fatal("invalid context found")
+	}
+}
+
+func TestFindParameter(testing *testing.T) {
+	ctx := FindContext(session, "testing", contexts)
+	param := FindParameter("name", ctx.Parameters)
+	if param == nil {
+		testing.Fatal("invalid param found")
+	}
+}
+
+func TestNewTextMessage(testing *testing.T) {
+	msg := NewTextMessage([]string{"Hey"})
+	if msg == nil {
+		testing.Fatal("invalid text message")
+	}
+}
+
+func TestNewCardButton(testing *testing.T) {
+	cbtn := NewCardButton("Test", "Subtest")
+	if cbtn == nil {
+		testing.Fatal("invalid card button")
+	}
+}
+
+func TestNewCardMessage(testing *testing.T) {
+	cbtn := NewCardButton("Test", "Subtest")
+	msg := NewCardMessage([]*dialogflow.Intent_Message_Card_Button{cbtn}, "Test", "Subtest", "")
+	if msg == nil {
+		testing.Fatal("invalid card message")
+	}
+}
+
+func TestNewTextValue(testing *testing.T) {
+	txtv := NewTextValue("test")
+	if txtv == nil || txtv.GetStringValue() != "test" {
+		testing.Fatal("invalid text value")
+	}
+}
+
+func TestNewNumberValue(testing *testing.T) {
+	txtv := NewNumberValue(500)
+	if txtv == nil || txtv.GetNumberValue() != 500 {
+		testing.Fatal("invalid number value")
+	}
+}
+
+func TestNewBoolValue(testing *testing.T) {
+	txtv := NewBoolValue(true)
+	if txtv == nil || txtv.GetBoolValue() != true {
+		testing.Fatal("invalid bool value")
+	}
+}
+
+func TestNewTextMessageCollection(testing *testing.T) {
+	msgc := NewTextMessageCollection([]string{"test"})
+	if msgc == nil {
+		testing.Fatal("invalid text message collection")
+	}
+}
